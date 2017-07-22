60384e51c33f64cf9920e56b02a4bd2df8e4e951
Dennis Iversen <dennis.iversen@gmail.com>
2017-07-22 12:44:14 +0200

    auto disconnect of gain node

--- dist/adsr-gain-node.js
+++ dist/adsr-gain-node.js
@@ -12,38 +12,54 @@ function Gain(ctx) {
         maxGain: 1.0, // Max gain on note
         attackTime: 0.1, // AttackTime. gain.init to gain.max in attackTime
         sustainTime: 1, // Sustain note in time
-        releaseTime: 1 // Approximated end time. Calculated with secondsToTimeConstant()
+        releaseTime: 1, // Approximated end time. Calculated with secondsToTimeConstant()
+        // disconnect: false // Should we autodisconnect. Default is true
     };
 
     this.setOptions = function (options) {
         this.options = options;
     };
 
+    this.gainNode;
     /**
      * The gainNode
-     * @param {float} begin
-     * @param {float} endBegin
-     * @param {float} length
+     * @param {float} begin ctx time
      * @returns {Gain.getGainNode.gainNode}
      */
     this.getGainNode = function (begin) {
 
-        var gainNode = this.ctx.createGain();
-        gainNode.gain.value = this.options.initGain;
+        this.gainNode = this.ctx.createGain();
+        this.gainNode.gain.value = this.options.initGain;
 
         // Attack to max
-        gainNode.gain.setTargetAtTime(
+        this.gainNode.gain.setTargetAtTime(
                 this.options.maxGain,
                 begin + this.options.attackTime,
                 this.options.attackTime);
 
         // Sustain and end note
-        gainNode.gain.setTargetAtTime(
+        this.gainNode.gain.setTargetAtTime(
                 0.0,
                 begin + this.options.attackTime + this.options.sustainTime,
                 this.secondsToTimeConstant(this.options.releaseTime));
-
-        return gainNode;
+        
+        if (this.options.disconnect !== false) {
+            this.disconnect(begin);
+        }
+        
+        return this.gainNode;
+    };
+    
+    this.disconnect = function() {
+        var totalLength = 
+                this.options.attackTime + this.options.sustainTime + this.options.releaseTime;
+        
+        setTimeout( () => {
+            // console.log(this.gainNode);
+            this.gainNode.disconnect();
+            console.log(totalLength);
+        },
+        totalLength * 1000);
     };
 }
 
